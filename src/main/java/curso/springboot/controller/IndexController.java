08b00bf740ed15724874ba8c369a368f1c62c969
Israel Andrade <israelg.andrade@bol.com.br>
2020-11-23 11:56:35 -0300

    Correção login

--- src/main/java/curso/springboot/controller/IndexController.java
+++ src/main/java/curso/springboot/controller/IndexController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 	
 	@RequestMapping(value = "/")
-	public String index() {
-		return "index";
-	}
+	public String index() { return "index"; }
+	
+	@RequestMapping(value = "/login")
+	public String login() { return "login";	}
 
 }
